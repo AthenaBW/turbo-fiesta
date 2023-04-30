5e2b921afd9da95b3e0d99b8ed698dce1b2250a0
Athena <athenawilbon1994@aol.com>
2023-04-30 17:58:58 -0400

    Comments added to server.js and schema files

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,13 @@
+-- Drops the inventory_db if it exists currently --
 DROP DATABASE IF EXISTS employeetracker_db;
+
+-- Creates the inventory_db database --
 CREATE DATABASE employeetracker_db;
 
+-- use employeetracker_db database --
 USE employeetracker_db;
 
+-- Creates employee table --
 CREATE TABLE employee(
 	id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
@@ -12,6 +17,7 @@ CREATE TABLE employee(
     PRIMARY KEY (id)
 );
 
+-- Creates role table --
 CREATE TABLE role(
 	id INT AUTO_INCREMENT NOT NULL,
 	title VARCHAR(30) NOT NULL,
@@ -19,7 +25,7 @@ CREATE TABLE role(
   department_id INT NOT NULL,
     PRIMARY KEY (id)
 );
-
+-- Creates department table --
 CREATE TABLE department(
 	id INT AUTO_INCREMENT NOT NULL,
 	name VARCHAR(30) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-
+-- Depratments --
 INSERT INTO department(name)
 VALUES ('Human Resources'),
        ('Telemarketing'),
@@ -7,7 +7,7 @@ VALUES ('Human Resources'),
        ('Graphic Design'),
        ('Finance');
 
--- Finance roles
+-- Finance roles --
 INSERT INTO role(title, salary, department_id)
 VALUES 
   ("Financial Advisor", 18000, 00),
@@ -16,33 +16,33 @@ VALUES
   ("Financial Trainee", 10000, 00),
   ("Financial Employee", 14000, 00),
   ("Financial Director", 19000, 00),
--- Graphic Design roles
+-- Graphic Design roles --
   ("Graphic Design Advisor", 18000, 01),
   ("Graphic Design Assistant", 13000, 01),
   ("Graphic Design Head", 20000, 01),
   ("Graphic Design Trainee", 10000, 01),
   ("Graphic Design Employee", 14000, 01),
   ("Graphic Design Director", 19000, 01),
---   Production Roles
+--   Production Roles --
   ("Prouction Advisor", 19000, 02),
   ("Production  Assistant", 12000, 02),
   ("Production Head", 24000, 02),
   ("Production Trainee", 11000, 02),
   ("Production Employee", 15000, 02),
   ("Production Director", 21000, 02),
---   Marketing Roles
+--   Marketing Roles --
   ("Marketing Head", 18000, 03),
   ("Marketing Assistant", 13000, 03),
   ("Marketing Trainee", 20000, 03),
   ("Marketing Employee", 10000, 03),
   ("Marketing Director", 14000, 03),
---   Telemarkting Roles
+--   Telemarkting Roles --
   ("Telemarketing Head", 18000, 03),
   ("Telemarketing Assistant", 13000, 03),
   ("Telemarketing Trainee", 20000, 03),
   ("Telemarketing Employee", 10000, 03),
   ("Telemarketing Director", 14000, 03),
---  Human Resource Roles 
+--  Human Resource Roles  --
   ("Human Resource Head", 18000, 04),
   ("Human Resource Assistant", 13000, 04),
   ("Human Resource Trainee", 20000, 04),
