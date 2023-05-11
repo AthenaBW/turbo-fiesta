bebed8f433fb43f097eec51c2053c5f9974b444c
Athena <athenawilbon1994@aol.com>
2023-05-10 20:47:16 -0400

    pw taken down

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ class DatabaseManager {
       host: "localhost",
       port: 3306,
       user: "root",
-      password: "Braylin77play!",
+      password: 
       database: "employeetracker_db"
     });
   }
